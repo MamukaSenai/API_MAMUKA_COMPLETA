3566632e737b0ce3c2eda3349164b3f8662fa77a
Richard Fernandes Passarelli <45811737874@info.local>
2024-02-09 15:25:09 -0300

    Finalizada API--NAO MEXER, TO CANSADO DE ERRO, AAAAAAAAAHHHHHHHH

---
+++ apimamuka_atualizado_completo/src/main/java/com/mamuka/apimamuka/repositories/TarefaRepository.java
@@ -0,0 +1,16 @@
+package com.mamuka.apimamuka.repositories;
+
+import com.mamuka.apimamuka.models.TarefaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+import java.util.List;
+import java.util.UUID;
+
+
+public interface TarefaRepository extends JpaRepository<TarefaModel, UUID> {
+
+    List<TarefaModel> findByProjetoId(UUID idProjeto);
+
+}
