92cfc21e91193816c60b8f83e3bac15c6dabdf19
Vinicius-Zaneti <viniciuszaneti.vz@gmail.com>
2024-02-17 20:08:29 -0300

    ajustando o SecurityConfig

--- apimamuka_atualizado_completo/src/main/java/com/mamuka/apimamuka/config/SecurityConfig.java
+++ apimamuka_atualizado_completo/src/main/java/com/mamuka/apimamuka/config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.POST, "/projetos").hasRole("ADMIN")
+//                        .requestMatchers(HttpMethod.POST, "/projetos").hasRole("ADMIN")
                         // .requestMatchers(HttpMethod.POST, "/servicos").hasRole("CLIENTE") // para fazer no servicos somente CLIENTE
                         //.requestMatchers(HttpMethod.POST).authenticated()
 
